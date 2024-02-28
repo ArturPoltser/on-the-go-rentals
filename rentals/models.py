7279bd4999cb2bd35be5461c3331252efb6a4603
Artur Poltser <polcerartur@gmail.com>
2024-02-28 16:04:45 +0200

    add insurance model

--- rentals/models.py
+++ rentals/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Insurance(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
