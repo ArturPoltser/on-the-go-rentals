f57a8bdd9f6826ebd3835869b969f055dc5d0965
Artur Poltser <polcerartur@gmail.com>
2024-02-28 18:09:45 +0200

    create temporary login and logout html

--- on_the_go_rentals/settings.py
+++ on_the_go_rentals/settings.py
@@ -101,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = "user.Renter"
+AUTH_USER_MODEL = "rentals.Renter"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
