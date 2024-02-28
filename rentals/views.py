b37e2d63970ea311378ee98c70d8e3cf44824ebe
Artur Poltser <polcerartur@gmail.com>
2024-02-28 17:47:34 +0200

    create index view

--- rentals/views.py
+++ rentals/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from rentals.models import Renter, Car, Insurance
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_renters = Renter.objects.count()
+    num_cars = Car.objects.count()
+    num_insurances = Insurance.objects.count()
+
+    context = {
+        "num_renters": num_renters,
+        "num_cars": num_cars,
+        "num_insurances": num_insurances,
+    }
+
+    return render(request, "rentals/index.html", context=context)
